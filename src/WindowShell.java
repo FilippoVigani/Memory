7a1d78ed7f67d9151ae1d177644f2b5cdad14db3
VeegaP <pippoviga1995@gmail.com>
2015-03-24 15:00:51 +0100

    Window Shell containing the different UI controls which can be changed dynamically.

---
+++ src/WindowShell.java
@@ -0,0 +1,15 @@
+import java.awt.HeadlessException;
+import java.awt.Window;
+
+/**
+ * @author Filippo Vigani
+ *
+ */
+public class WindowShell extends Window {
+
+	public WindowShell() throws HeadlessException {
+		super(null);
+		// TODO Auto-generated constructor stub
+	}
+
+}
