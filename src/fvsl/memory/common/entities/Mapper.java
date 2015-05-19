1b379e118f49775f8909fc16f222003fbe9866bd
sleggio <sleggio@sleggio-PC>
2015-05-19 18:30:59 +0200

    caricato map

--- src/fvsl/memory/common/entities/Mapper.java
+++ src/fvsl/memory/common/entities/Mapper.java
@@ -1,15 +1,25 @@
 package fvsl.memory.common.entities;
 
+import java.io.File;
 import java.util.HashMap;
 
 public class Mapper {
 	private HashMap<String, String> cardsMap;
 	
 	private static Mapper mapper;
+	private File folder = new File("/Memory/res");
+	private File[] listOfFiles = folder.listFiles();
+
 	
 	public Mapper(){
 		cardsMap = new HashMap<String, String>();
-		cardsMap.put("c1", "smile.jpg");
+		 for (int i = 0; i < listOfFiles.length; i++) {
+		      if (listOfFiles[i].isFile()) {
+		        System.out.println("File " + listOfFiles[i].getName());
+		        cardsMap.put(("c"+i), listOfFiles[i].getName());
+		      } 
+		    }
+		
 	}
 	
 	public static Mapper getMapper(){
